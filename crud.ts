d51cfd0c42e369c92fb5739a940df0dcad8011bd
md-alamin2 <mdalamin22671@gmail.com>
2026-02-16 23:31:53 +0600

    learn how to read data in prisma

--- crud.ts
+++ crud.ts
@@ -1,35 +1,51 @@
 import { prisma } from "./lib/prisma";
 
 async function run() {
-    const createUser = await prisma.user.create({
-        data:{
-            name: "Al-amin",
-            email: "alamin@gmail.com"
+    // const createUser = await prisma.user.create({
+    //     data:{
+    //         name: "Al-amin",
+    //         email: "alamin@gmail.com"
+    //     }
+    // })
+
+    // console.log(createUser)
+
+    // // create post for user id: 4
+    // const createPost = await prisma.post.create({
+    //     data:{
+    //         title: "Hello World",
+    //         content: "I am learning prisma orm",
+    //         authorId: 4
+    //     }
+    // })
+    // console.log(createPost)
+
+
+    // // create profile fro user id: 4
+    // const createProfile = await prisma.profile.create({
+    //     data:{
+    //         bio: "I am MERN stack web developer",
+    //         userId: 4
+    //     }
+    // })
+    // console.log(createProfile)
+
+    // get all user
+    const users = await prisma.user.findMany({
+        // include:{
+        //     profile: true,
+        //     posts: true
+        // }
+
+        select:{
+            id:true,
+            name: true,
+            email: true,
+            profile:true,
+            posts: true
         }
-    })
-
-    console.log(createUser)
-
-    // create post for user id: 4
-    const createPost = await prisma.post.create({
-        data:{
-            title: "Hello World",
-            content: "I am learning prisma orm",
-            authorId: 4
-        }
-    })
-    console.log(createPost)
-
-
-    // create profile fro user id: 4
-    const createProfile = await prisma.profile.create({
-        data:{
-            bio: "I am MERN stack web developer",
-            userId: 4
-        }
-    })
-    console.log(createProfile)
-
+    });
+    console.dir(users, {depth: Infinity})
 
 
 }
